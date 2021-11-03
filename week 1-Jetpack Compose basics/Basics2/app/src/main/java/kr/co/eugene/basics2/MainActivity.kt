567d3e0be14d75a2ab332c46b997d8b5395b9321
Eugene Lim <eugenelgm@gmail.com>
2021-11-03 14:03:23 +0900

    create my project!

---
+++ week 1-Jetpack Compose basics/Basics2/app/src/main/java/kr/co/eugene/basics2/MainActivity.kt
@@ -0,0 +1,40 @@
+package kr.co.eugene.basics2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.co.eugene.basics2.ui.theme.Basics2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Basics2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Basics2Theme {
+        Greeting("Android")
+    }
+}
